5192598d49e67d76b83eb786a64c988ba13e5d23
Natalia Kirdis <kirdisnatalia@gmail.com>
2021-06-30 10:28:47 +0300

    Added steps for result page

--- src/test/java/ru/yandex/pages/SearchResultsPage.java
+++ src/test/java/ru/yandex/pages/SearchResultsPage.java
@@ -1,7 +1,7 @@
 package ru.yandex.pages;
 
 import com.codeborne.selenide.ElementsCollection;
-//import io.qameta.allure.Step;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.exactText;
@@ -12,12 +12,12 @@ public class SearchResultsPage {
   ElementsCollection results = $$("li.serp-item");
   ElementsCollection urls = $$("div.Path b");
 
-  //@Step("Check that there is a result with url {url}")
+  @Step("Check that there is a result with url {url}")
   public void haveUrl(String url) {
     urls.filter(exactText(url)).shouldHave(sizeGreaterThan(1));
   }
 
-  //@Step("Check that there is at least {expectedSize} result")
+  @Step("Check that there is at least {expectedSize} result")
   public void sizeIsAtLeast(int expectedSize) {
     results.shouldHave(sizeGreaterThan(expectedSize));
   }
